292dd0225207d41c1398e552127fbff0409f1b17
faziamebrek <faziamebrek@gmail.com>
2024-09-25 21:15:22 -0700

    EvenOdd

---
+++ src/fazia/EvenOdd.java
@@ -0,0 +1,23 @@
+package fazia;
+
+public class EvenOdd {
+
+    public void PrintEvenOdd (int a){
+
+    if (a % 2 == 0) {
+        System.out.println(a+ " is an even Number ");
+
+    } else {
+        System.out.println( a+ " is an Odd Number");
+    }
+
+}
+
+    public static void main(String[] args) {
+        EvenOdd evenOdd = new EvenOdd();
+        evenOdd.PrintEvenOdd(30);
+
+    }
+}
+
+
